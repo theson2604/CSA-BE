be8025729eb743b2c453afa28ba14a08c7c7910a
Son Hoang <theson2604@gmail.com>
2024-04-25 10:52:12 +0700

    add process_fields func to avoid redundancy in create_record and upate_record

--- RecordObject/services.py
+++ RecordObject/services.py
@@ -23,6 +23,12 @@ class RecordException(Exception):
 
 class IRecordObjectService(ABC):
     @abstractmethod
+    async def process_fields(
+        self, fields: dict, record: dict, obj_id: str
+    ) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
     async def create_record(
         self, record: RecordObjectSchema, current_user_id: str
     ) -> str:
@@ -63,13 +69,10 @@ class RecordObjectService(IRecordObjectService):
         self.object_repo = ObjectRepository(db_str) 
         self.elastic_service = ElasticsearchRecord(db_str, obj_id_str, obj_id)
 
-    async def create_record(
-        self, record: RecordObjectSchema, current_user_id: str
-    ) -> str:
-        obj_id = record.pop("object_id")
-        inserted_record = {"object_id": obj_id}
-        
-        for field_id, field_value in record.items():
+    async def process_fields(
+        self, fields: dict, record: dict, obj_id: str
+    ) -> dict:
+        for field_id, field_value in fields.items():
             field_detail = await self.field_obj_repo.find_one_by_field_id(
                 obj_id, field_id
             )
@@ -146,7 +149,17 @@ class RecordObjectService(IRecordObjectService):
                     "field_value": ref_fld_id,
                 }
 
-            inserted_record.update({field_id: field_value})
+            record.update({field_id: field_value})
+
+        return record
+
+    async def create_record(
+        self, record: RecordObjectSchema, current_user_id: str
+    ) -> str:
+        obj_id = record.pop("object_id")
+        inserted_record = {"object_id": obj_id}
+        
+        inserted_record = await self.process_fields(record, inserted_record, obj_id)
 
         list_field_details = await self.field_obj_repo.get_all_by_field_types(obj_id, [FieldObjectType.ID])
         field_id_detail = list_field_details[0]
@@ -307,84 +320,7 @@ class RecordObjectService(IRecordObjectService):
         obj_id = record.pop("object_id")
         updated_record = {}
 
-        for field_id, field_value in record.items():
-            field_detail = await self.field_obj_repo.find_one_by_field_id(
-                obj_id, field_id
-            )
-            field_type = field_detail.get("field_type")
-            
-            if field_type == FieldObjectType.TEXT:
-                length = field_detail.get("length")
-                if not isinstance(field_value, str):
-                    raise RecordException(
-                        f"field_value '{field_value}' of type {field_type} must be str."
-                    )
-                if len(field_value) > length:
-                    raise RecordException(f"len(field_value) must be < {length}")
-
-            elif field_type == FieldObjectType.EMAIL:
-                email_regex = (
-                    "^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$"
-                )
-                match = re.search(email_regex, field_value)
-                if not match:
-                    raise RecordException(
-                        f"field_value '{field_value}' of type {field_type} must match email_regex {email_regex}"
-                    )
-
-            elif field_type == FieldObjectType.PHONE_NUMBER:
-                country_code = field_detail.get("country_code")
-                if country_code == "+84":
-                    vn_phone_regex = "^(0|84)(2(0[3-9]|1[0-6|8|9]|2[0-2|5-9]|3[2-9]|4[0-9]|5[1|2|4-9]|6[0-3|9]|7[0-7]|8[0-9]|9[0-4|6|7|9])|3[2-9]|5[5|6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])([0-9]{7})$"
-                    match = re.search(vn_phone_regex, field_value)
-                    if not match:
-                        raise RecordException(
-                            f"field_value '{field_value}' of type {field_type} must be match vn_phone_regex {vn_phone_regex}"
-                        )
-
-            elif field_type == FieldObjectType.SELECT:
-                options = field_detail.get("options")
-                if field_value not in options:
-                    raise RecordException(
-                        f"field_value '{field_value}' of type {field_type} must exist in options {options}"
-                    )
-
-            elif field_type == FieldObjectType.REFERENCE_OBJECT:
-                ref_obj_id = field_detail.get("ref_obj_id")  # obj_<name>_<id>
-                # obj_id's record repo
-                ref_record_repo = RecordObjectRepository(self.db_str, ref_obj_id)
-                ref_record = await ref_record_repo.find_one_by_id(field_value)
-                if not ref_record:
-                    raise RecordException(
-                        f"Not found ref record '{field_value}' in {ref_obj_id}"
-                    )
-
-                obj_detail = await self.object_repo.find_one_by_object_id(ref_obj_id)
-                field_ids = await self.field_obj_repo.get_all_by_field_types(obj_detail.get("_id"), [FieldObjectType.ID.value])
-                
-                if field_ids and len(field_ids) == 1:
-                    field_value = {"ref_to": ref_record.get("_id"), "field_value": field_ids[0].get("field_id")}
-
-            elif field_type == FieldObjectType.REFERENCE_FIELD_OBJECT:
-                ref_field_obj_id = field_detail.get(
-                    "ref_field_obj_id"
-                )  # obj_<name>_<id>.fd_<name>_<id>
-                splitted = ref_field_obj_id.split(".")
-                ref_obj_id, ref_fld_id = splitted[0], splitted[1]
-                # obj_id's record repo
-                ref_record_repo = RecordObjectRepository(self.db_str, ref_obj_id)
-                ref_record = await ref_record_repo.find_one_by_id(field_value)
-                if not ref_record:
-                    raise RecordException(
-                        f"Not found ref record '{field_value} in {ref_obj_id}"
-                    )
-
-                field_value = {
-                    "ref_to": ref_record.get("_id"),
-                    "field_value": ref_fld_id,
-                }
-
-            updated_record.update({field_id: field_value})
+        updated_record = await self.process_fields(record, updated_record, obj_id)
         
         updated_record.update(
         {
