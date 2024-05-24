7dc1c06e6846da86f178443aa4125389b3d45ba0
quannh <quannh@basebs.com>
2024-05-24 11:58:02 +0700

    Fix ca.crt

--- app/common/elastic.py
+++ app/common/elastic.py
@@ -1,15 +1,22 @@
-import asyncio
-from elasticsearch import AsyncElasticsearch, Elasticsearch
+from elasticsearch import AsyncElasticsearch
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 class ElasticsearchBase:
     def __init__(self):
         self.es = AsyncElasticsearch(
             [os.environ.get("ELASTIC_HOST")],
-            basic_auth=(os.environ.get("ELASTIC_USERNAME"), os.environ.get("ELASTIC_PASSWORD")),
-            ca_certs=f"{os.environ.get("ROOT_DIR")}\ca.crt",
+            basic_auth=(
+                os.environ.get("ELASTIC_USERNAME"),
+                os.environ.get("ELASTIC_PASSWORD"),
+            ),
+            ca_certs=os.environ.get("CERT_FILE"),
+            timeout=60
         )
 
     async def health_check(self) -> bool:
